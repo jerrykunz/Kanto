7ca7b10e51fd8df0f266c46c9475b25d1a631e4d
Jeremia Kunnari <jeremia@gmail.com>
2021-02-10 15:58:28 +0200

    texture blending added

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -169,6 +169,7 @@ public:
 		m_TextureShader.reset(Kanto::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
 
 		m_Texture = Kanto::Texture2D::Create("res/textures/Checkerboard.png");
+		m_ChernoLogoTexture = Kanto::Texture2D::Create("res/textures/ChernoLogo.png");
 
 		std::dynamic_pointer_cast<Kanto::OpenGLShader>(m_TextureShader)->Bind();
 		std::dynamic_pointer_cast<Kanto::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
@@ -240,6 +241,9 @@ public:
 		m_Texture->Bind();
 		Kanto::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
+		m_ChernoLogoTexture->Bind();
+		Kanto::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+
 		// Triangle
 		// Hazel::Renderer::Submit(m_Shader, m_VertexArray);
 
@@ -266,7 +270,7 @@ public:
 		Kanto::Ref<Kanto::Shader> m_FlatColorShader, m_TextureShader;
 		Kanto::Ref<Kanto::VertexArray> m_SquareVA;
 
-		Kanto::Ref<Kanto::Texture2D> m_Texture;
+		Kanto::Ref<Kanto::Texture2D> m_Texture, m_ChernoLogoTexture;
 
 		Kanto::OrthographicCamera m_Camera;
 		glm::vec3 m_CameraPosition;
