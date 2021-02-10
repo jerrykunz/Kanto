ff924fc1911dfb73aebf0a1834d17edc540710c7
Jeremia Kunnari <jeremia@gmail.com>
2021-02-10 15:17:45 +0200

    added opengl shader and some other stuff

--- Kanto/src/Kanto/Renderer/Renderer.cpp
+++ Kanto/src/Kanto/Renderer/Renderer.cpp
@@ -1,6 +1,8 @@
 #include "knpch.h"
 #include "Renderer.h"
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 namespace Kanto {
 
 
@@ -17,10 +19,11 @@ namespace Kanto {
 	{
 	}
 
-	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray)
+	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
 	{
 		shader->Bind();
-		shader->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -1,24 +1,21 @@
 #include <Kanto.h>
+#include "Platform/OpenGL/OpenGLShader.h"
 #include  "ImGui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-//test
-#include <glm/vec3.hpp> // glm::vec3
-#include <glm/vec4.hpp> // glm::vec4
-#include <glm/mat4x4.hpp> // glm::mat4
-#include <glm/ext/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale
-#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
-#include <glm/ext/scalar_constants.hpp> // glm::pi
 
-glm::mat4 camera(float Translate, glm::vec2 const& Rotate)
-{
-	glm::mat4 Projection = glm::perspective(glm::pi<float>() * 0.25f, 4.0f / 3.0f, 0.1f, 100.f);
-	glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Translate));
-	View = glm::rotate(View, Rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
-	View = glm::rotate(View, Rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
-	glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
-	return Projection * View * Model;
-}
+//
+//glm::mat4 camera(float Translate, glm::vec2 const& Rotate)
+//{
+//	glm::mat4 Projection = glm::perspective(glm::pi<float>() * 0.25f, 4.0f / 3.0f, 0.1f, 100.f);
+//	glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Translate));
+//	View = glm::rotate(View, Rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
+//	View = glm::rotate(View, Rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
+//	glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
+//	return Projection * View * Model;
+//}
 
 
 class ExampleLayer : public Kanto::Layer
@@ -55,10 +52,10 @@ public:
 		m_SquareVA.reset(Kanto::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<Kanto::VertexBuffer> squareVB;
@@ -82,6 +79,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -90,7 +88,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -109,38 +107,42 @@ public:
 
 
 
-		m_Shader.reset(new Kanto::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(Kanto::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 			out vec3 v_Position;
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 			in vec3 v_Position;
+			uniform vec3 u_Color;
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_BlueShader.reset(new Kanto::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(Kanto::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	virtual void OnImGuiRender() override
 	{
-		
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnUpdate(Kanto::Timestep ts) override
@@ -160,6 +162,18 @@ public:
 		if (Kanto::Input::IsKeyPressed(KN_KEY_D))
 			m_CameraRotation -= m_CameraRotationSpeed * ts;
 
+
+		if (Kanto::Input::IsKeyPressed(KN_KEY_J))
+			m_SquarePos.x -= m_SquareSpeed * ts;
+		else if (Kanto::Input::IsKeyPressed(KN_KEY_L))
+			m_SquarePos.x += m_SquareSpeed * ts;
+
+		if (Kanto::Input::IsKeyPressed(KN_KEY_I))
+			m_SquarePos.y += m_SquareSpeed * ts;
+		else if (Kanto::Input::IsKeyPressed(KN_KEY_K))
+			m_SquarePos.y -= m_SquareSpeed * ts;
+
+
 		Kanto::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 		Kanto::RenderCommand::Clear();
 
@@ -168,7 +182,25 @@ public:
 
 		Kanto::Renderer::BeginScene(m_Camera);
 
-		Kanto::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		std::dynamic_pointer_cast<Kanto::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<Kanto::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_SquarePos);
+		//Kanto::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Kanto::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
+			}
+		}
+
+
 		Kanto::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Kanto::Renderer::EndScene();
@@ -189,15 +221,20 @@ public:
 		std::shared_ptr<Kanto::Shader> m_Shader;
 		std::shared_ptr<Kanto::VertexArray> m_VertexArray;
 
-		std::shared_ptr<Kanto::Shader> m_BlueShader;
+		std::shared_ptr<Kanto::Shader> m_FlatColorShader;
 		std::shared_ptr<Kanto::VertexArray> m_SquareVA;
 
 		Kanto::OrthographicCamera m_Camera;
 		glm::vec3 m_CameraPosition;
 		float m_CameraMoveSpeed = 5.0f;
 
+		glm::vec3 m_SquarePos = { 0.0f, 0.0f, 0.0f };
+		float m_SquareSpeed = 3.5f;
+
 		float m_CameraRotation = 0.0f;
 		float m_CameraRotationSpeed = 180.0f;
+
+		glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 
